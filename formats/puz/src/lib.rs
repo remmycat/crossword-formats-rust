6023f245a6ca2767f474b8740521cf8fa6a62572
Remmy Cat Stock <3317423+remmycat@users.noreply.github.com>
2023-09-30 16:47:08 +0200

    little a setup (as a treat)

--- formats/puz/src/lib.rs
+++ formats/puz/src/lib.rs
@@ -1,5 +1,49 @@
-pub fn add(left: usize, right: usize) -> usize {
-	left + right
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ParsePuzError {
+	#[error("error variants wip")]
+	ToDo,
+}
+
+#[derive(Debug)]
+pub enum PuzzleType {
+	Normal,
+	Diagramless,
+}
+
+#[derive(Debug)]
+pub enum SolutionState {
+	Unlocked,
+	Scrambled,
+}
+
+#[derive(Debug)]
+pub struct PuzFileHeader {
+	// pub preamble: Vec<u8>,
+	// pub checksum_overall: u16,
+	// pub checksum_cib: u16,
+	// pub masked_checksums: [u8; 8],
+	// pub version: String,
+	// pub unknown_data_1: [u8; 2],
+	// pub scrambled_checksum: u16,
+	// pub unknown_data_2: [u8; 12],
+	// pub width: u8,
+	// pub height: u8,
+	// pub clue_count: u16,
+	// pub puzzle_type: PuzzleType,
+	// pub solution_state: SolutionState,
+}
+
+#[derive(Debug)]
+pub struct PuzFile {
+	pub header: PuzFileHeader,
+}
+
+pub fn parse_a_puz(_puz_bytes: &[u8]) -> Result<PuzFile, ParsePuzError> {
+	Ok(PuzFile {
+		header: PuzFileHeader {},
+	})
 }
 
 #[cfg(test)]
@@ -7,8 +51,13 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn it_works() {
-		let result = add(2, 2);
-		assert_eq!(result, 4);
+	fn it_parses_unicode_puz() {
+		// This will break compilation if you don't have an example file there, sorry!
+		// I will provide some in the future.
+		let unicode_puzzle = include_bytes!("../fixtures/unicode.puz");
+
+		let parsed = parse_a_puz(unicode_puzzle).expect("Parsing Failed");
+
+		dbg!(parsed);
 	}
 }
